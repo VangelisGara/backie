66a61372ba32bcd336ed5f7096f401c644fd13ff
medev <developpmentt@gmail.com>
2021-07-13 23:37:15 +0300

    Add ETA to groovy script

--- backie.groovy
+++ backie.groovy
@@ -1,7 +1,8 @@
 @Grab(group='commons-io', module='commons-io', version='2.6')
 
-import static groovy.io.FileType.FILES
+import java.util.concurrent.TimeUnit
 import org.apache.commons.io.FileUtils
+import static groovy.io.FileType.FILES
 
 String sourceFolderPath, targetFolderPath
 Boolean shouldDeleteBackedUpFiles = false
@@ -20,20 +21,18 @@ if(shouldDeleteBackedUpFiles) {
    if(answer != "y") System.exit(0)
 }
 
-List<String> filesNotBackedUp = []
-
 String date = new Date().format('yyyy-MM-dd hh-mm')
 String logFileName = "backie_${date}.out"
 File logFile = new File(logFileName)
 
 def printProgress = { long startTime, long total, long current ->
-    //long eta = current == 0 ? 0 : 
-      //  (total - current) * (System.currentTimeMillis() - startTime) / current;
+    long eta = current == 0 ? 0 : (total - current) * (System.currentTimeMillis() - startTime) / current;
 
-    //String etaHms = current == 0 ? "N/A" : 
-      //      String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(eta),
-        //            TimeUnit.MILLISECONDS.toMinutes(eta) % TimeUnit.HOURS.toMinutes(1),
-          //          TimeUnit.MILLISECONDS.toSeconds(eta) % TimeUnit.MINUTES.toSeconds(1));
+    String etaHms = current == 0 
+	? "N/A" 
+	: String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(eta),
+             TimeUnit.MILLISECONDS.toMinutes(eta) % TimeUnit.HOURS.toMinutes(1),
+             TimeUnit.MILLISECONDS.toSeconds(eta) % TimeUnit.MINUTES.toSeconds(1));
 
     StringBuilder string = new StringBuilder(140);   
     int percent = (int) (current * 100 / total);
@@ -45,29 +44,39 @@ def printProgress = { long startTime, long total, long current ->
         .append('>')
         .append(String.join("", Collections.nCopies(100 - percent, " ")))
         .append(']')
-        .append(String.join("", Collections.nCopies(current == 0 ? (int) (Math.log10(total)) : (int) (Math.log10(total)) - (int) (Math.log10(current)), " ")))
-        //.append(String.format(" %d/%d, ETA: %s", current, total, etaHms));
+        .append(String.join(
+	   "", 
+           Collections.nCopies(current == 0 
+              ? (int) (Math.log10(total)) 
+              : (int) (Math.log10(total)) - (int) (Math.log10(current)), " "
+            )
+         ))
+        .append(String.format(" %d/%d, ETA: %s", current, total, etaHms));
 
     System.out.print(string);
 }
 
 println "\nInspecting which files from destination ${sourceFolderPath} is backed up to target destination ${targetFolderPath}...\n"
 logFile << "\nSource File Destination[${sourceFolderPath}]   Target File Destination(s)[${targetFolderPath}]\n\n"
-int current = 0;
 
+int current = 0;
 int total = 0;
+
 new File(sourceFolderPath).traverse(type: groovy.io.FileType.FILES) { it ->
    total = total + 1
 }
 
+long startTime = System.currentTimeMillis();
+
+List<String> filesNotBackedUp = []
+
 new File(sourceFolderPath).traverse(type: groovy.io.FileType.FILES) { source_it ->
-    printProgress(0,total,current)
+    printProgress(startTime,total,current)
     current = current + 1
     String currentSourceFileName = source_it.toString()
     File currentSourceFile = new File(currentSourceFileName)
     logFile << currentSourceFileName + "   "
     Boolean found = false
-    System.in.newReader().readLine()
     new File(targetFolderPath).traverse(type: groovy.io.FileType.FILES) { target_it ->
 	String currentTargetFileName = target_it.toString()
         File currentTargetFile = new File(currentTargetFileName)
