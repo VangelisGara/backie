eeb47883794dac2722558e79bdd20cfef25b9dbd
medev <developpmentt@gmail.com>
2021-07-19 23:30:01 +0300

    Clean and refactor code the groovy way

--- backie.groovy
+++ backie.groovy
@@ -31,37 +31,29 @@ String logFileNameForNotBackedUp = "${outputDirName}/not_backed_up_files.out"
 File logFileForBackedUp = new File(logFileNameForBackedUp)
 File logFileForNotBackedUp = new File(logFileNameForNotBackedUp)
 
-def printProgress = { long startTime, long total, long current ->
-    StringBuilder string = new StringBuilder(140);   
-    int percent = (int) (current * 100 / total);
-    string
-        .append('\r')
-        .append(String.join("", Collections.nCopies(percent == 0 ? 2 : 2 - (int) (Math.log10(percent)), " ")))
-        .append(String.format(" %d%% [", percent))
-        .append(String.join("", Collections.nCopies(percent, "=")))
-        .append('>')
-        .append(String.join("", Collections.nCopies(100 - percent, " ")))
-        .append(']')
-        .append(String.join(
-	   "", 
-           Collections.nCopies(current == 0 
-              ? (int) (Math.log10(total)) 
-              : (int) (Math.log10(total)) - (int) (Math.log10(current)), " "
-            )
-         ))
-        .append(String.format(" %d/%d", current, total));
+def printProgress = { long current, long total ->
+	StringBuilder string = new StringBuilder(140)   
+	int percent = (int) (current * 100 / total)
+	string
+		.append('\r')
+		.append(" "*(percent == 0 ? 2 : 2 - (int) (Math.log10(percent))))
+		.append(String.format("%d%% ", percent))
+		.append("."*percent)
+		.append(" "*(100-percent))
+		.append(" "*(current == 0 ? (int) (Math.log10(total)) : (int) (Math.log10(total)) - (int) (Math.log10(current))))
+        	.append(String.format(" %d/%d", current, total))
 
-    System.out.print(string);
+	System.out.print(string)
 }
 
-println "Inspecting which files from destination [${sourceFolderPath}] is backed up to target destination [${targetFolderPath}]..."
+println "Inspecting which files from destination [${sourceFolderPath}] is backed up to target destination [${targetFolderPath}]...\n"
 logFileForBackedUp << "\nSource File Destination[${sourceFolderPath}]   Target File Destination(s)[${targetFolderPath}]\n\n"
 
 int current = 0;
 int total = 0;
 
 new File(sourceFolderPath).traverse(type: groovy.io.FileType.FILES) { it ->
-   total = total + 1
+	total = total + 1
 }
 
 long startTime = System.currentTimeMillis();
@@ -69,37 +61,37 @@ long startTime = System.currentTimeMillis();
 List<String> filesNotBackedUp = []
 
 new File(sourceFolderPath).traverse(type: groovy.io.FileType.FILES) { source_it ->
-    printProgress(startTime,total,current)
-    current = current + 1
-    String currentSourceFileName = source_it.toString()
-    File currentSourceFile = new File(currentSourceFileName)
-    Boolean found = false
-    List<String> whereIsBackedUp = []
-    new File(targetFolderPath).traverse(type: groovy.io.FileType.FILES) { target_it ->
-	String currentTargetFileName = target_it.toString()
-        File currentTargetFile = new File(currentTargetFileName)
-        if(FileUtils.contentEquals(currentSourceFile, currentTargetFile)) {
-            found = true
-	    whereIsBackedUp.add(currentTargetFileName)
-        }
-    }
-    if(!found) filesNotBackedUp.add(currentSourceFileName)
-    else {
-       if(shouldDeleteBackedUpFiles) {
-          logFileForBackedUp << "\nDeleting file from source target..."
-          currentSourceFile.delete()
-       }
-       logFileForBackedUp << currentSourceFileName + "   " + whereIsBackedUp +"\n"
-    } 
+	printProgress(current, total)
+	current = current + 1
+	String currentSourceFileName = source_it.toString()
+	File currentSourceFile = new File(currentSourceFileName)
+	Boolean found = false
+	List<String> whereIsBackedUp = []
+	new File(targetFolderPath).traverse(type: groovy.io.FileType.FILES) { target_it ->
+		String currentTargetFileName = target_it.toString()
+		File currentTargetFile = new File(currentTargetFileName)
+		if(FileUtils.contentEquals(currentSourceFile, currentTargetFile)) {
+			found = true
+			whereIsBackedUp.add(currentTargetFileName)
+		}
+	}
+	if(!found) filesNotBackedUp.add(currentSourceFileName)
+	else {
+		if(shouldDeleteBackedUpFiles) {
+			logFileForBackedUp << "\nDeleting file from source target..."
+			currentSourceFile.delete()
+		}
+		logFileForBackedUp << currentSourceFileName + "   " + whereIsBackedUp +"\n"
+	} 
 }
 
-printProgress(0,total,total)
+printProgress(total,total)
 
-println "\nAlmost done..."
+println "\n\nAlmost done..."
 
 logFileForNotBackedUp << "\n\n\nSource target's files that have not beed backed up\n\n"
 filesNotBackedUp.forEach { 
-   logFileForNotBackedUp << it + "\n"
+	logFileForNotBackedUp << it + "\n"
 }
 
 println "Inspection complete...See output files in folder: ${outputDirName}"
