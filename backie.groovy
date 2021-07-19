de017aa8254a1e6c62ece4fa83d9a73baaacb361
medev <developpmentt@gmail.com>
2021-07-19 23:49:46 +0300

    Add comments and remove unecessary imports

--- backie.groovy
+++ backie.groovy
@@ -1,36 +1,51 @@
 @Grab(group='commons-io', module='commons-io', version='2.6')
 
-import java.util.concurrent.TimeUnit
 import org.apache.commons.io.FileUtils
 import static groovy.io.FileType.FILES
 
+// Get command line arguments
 String sourceFolderPath, targetFolderPath
 Boolean shouldDeleteBackedUpFiles = false
 this.args.eachWithIndex { arg, indx ->
-    if(arg == "-s") sourceFolderPath = this.args[indx + 1]
-    if(arg == "-t") targetFolderPath = this.args[indx + 1]
-    if(arg == "-d") shouldDeleteBackedUpFiles = this.args[indx + 1].toBoolean()
+	if(arg == "-s") sourceFolderPath = this.args[indx + 1]
+	if(arg == "-t") targetFolderPath = this.args[indx + 1]
+	if(arg == "-d") shouldDeleteBackedUpFiles = this.args[indx + 1].toBoolean()
 }
 
-if(!sourceFolderPath) { println "Specify the source folder with -s flag..."; System.exit(0) }
-if(!targetFolderPath) { println "Specify the target folder with -t flag..."; System.exit(0) }
+// Error handling for user input
+if(!sourceFolderPath) { 
+	println "Specify the source folder with -s flag..."; 
+	System.exit(0) 
+}
+
+if(!targetFolderPath) { 
+	println "Specify the target folder with -t flag..."; 
+	System.exit(0)
+}
+
 if(shouldDeleteBackedUpFiles) { 
-   println "Backie will delete source files that have already been bucked up in target directory"
-   println("Are you sure that you want to proceed? (y|n)")
-   char answer = System.in.newReader().readLine() as char
-   if(answer != "y") System.exit(0)
+	println "Backie will delete source files that have already been bucked up in target directory"
+	println("Are you sure that you want to proceed? (y|n)")
+	char answer = System.in.newReader().readLine() as char
+	if(answer != "y") System.exit(0)
+	
 }
 
 
+// Create the log files
 String date = new Date().format('yyyy-MM-dd hh-mm-ss')
 String outputDirName = "backie_output_${date}"
+
 File outputDir = new File(outputDirName)
 outputDir.mkdir()
+
 String logFileNameForBackedUp = "${outputDirName}/backed_up_files.out"
 String logFileNameForNotBackedUp = "${outputDirName}/not_backed_up_files.out"
+
 File logFileForBackedUp = new File(logFileNameForBackedUp)
 File logFileForNotBackedUp = new File(logFileNameForNotBackedUp)
 
+// The status bar closure
 def printProgress = { long current, long total ->
 	StringBuilder string = new StringBuilder(140)   
 	int percent = (int) (current * 100 / total)
@@ -46,47 +61,65 @@ def printProgress = { long current, long total ->
 	System.out.print(string)
 }
 
-println "Inspecting which files from destination [${sourceFolderPath}] is backed up to target destination [${targetFolderPath}]...\n"
-logFileForBackedUp << "\nSource File Destination[${sourceFolderPath}]   Target File Destination(s)[${targetFolderPath}]\n\n"
-
+// Initialization phase
 int current = 0;
 int total = 0;
 
+// Count total number of files in source directory
 new File(sourceFolderPath).traverse(type: groovy.io.FileType.FILES) { it ->
 	total = total + 1
 }
 
-long startTime = System.currentTimeMillis();
+// Backie logic starts here
+println "Inspecting which files from destination [${sourceFolderPath}] is backed up to target destination [${targetFolderPath}]...\n"
+logFileForBackedUp << "\nSource File Destination[${sourceFolderPath}]   Target File Destination(s)[${targetFolderPath}]\n\n"
 
 List<String> filesNotBackedUp = []
 
+// For each file in source folder
 new File(sourceFolderPath).traverse(type: groovy.io.FileType.FILES) { source_it ->
+	
 	printProgress(current, total)
-	current = current + 1
+
 	String currentSourceFileName = source_it.toString()
 	File currentSourceFile = new File(currentSourceFileName)
+	
 	Boolean found = false
 	List<String> whereIsBackedUp = []
+	
+	// Check all files in target folder
 	new File(targetFolderPath).traverse(type: groovy.io.FileType.FILES) { target_it ->
+		
 		String currentTargetFileName = target_it.toString()
 		File currentTargetFile = new File(currentTargetFileName)
+		
+		// that have the same content
 		if(FileUtils.contentEquals(currentSourceFile, currentTargetFile)) {
 			found = true
 			whereIsBackedUp.add(currentTargetFileName)
 		}
+
 	}
-	if(!found) filesNotBackedUp.add(currentSourceFileName)
+
+	// Backup logic 
+	if(!found) { 
+		filesNotBackedUp.add(currentSourceFileName)
+	}
 	else {
 		if(shouldDeleteBackedUpFiles) {
 			logFileForBackedUp << "\nDeleting file from source target..."
 			currentSourceFile.delete()
 		}
-		logFileForBackedUp << currentSourceFileName + "   " + whereIsBackedUp +"\n"
-	} 
+		
+		logFileForBackedUp << currentSourceFileName + "   " + whereIsBackedUp + "\n"
+	}
+	
+	current = current + 1
 }
 
 printProgress(total,total)
 
+// Log not backed up files
 println "\n\nAlmost done..."
 
 logFileForNotBackedUp << "\n\n\nSource target's files that have not beed backed up\n\n"
@@ -94,4 +127,5 @@ filesNotBackedUp.forEach {
 	logFileForNotBackedUp << it + "\n"
 }
 
+// Final step
 println "Inspection complete...See output files in folder: ${outputDirName}"
